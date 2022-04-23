eb8c4339ef1be3cb6276830dd85c617c7ec0e185
kanikavarshney <kanika.varshney_cs19@gla.ac.in>
2022-04-24 00:44:27 +0530

    2asslevel1

---
+++ JavaScript_05.js
@@ -0,0 +1,104 @@
+//level 1
+
+
+//1-
+let firstName ='Kanika'
+let lastName = 'Varshney'
+let country = 'UttarPradesh'
+let city = 'Aligarh'
+let age = 21
+let isMarried = true
+let year = 2000
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
+
+//2-
+let aa ='10'
+let bb = 10
+console.log(typeof aa == typeof bb)
+
+3//-
+let num  = '9.81'
+let aaa = parseInt(num)
+console.log(aaa)
+let bbb = 10
+console.log(aaa == bbb)
+
+
+//4-truthy
+let ab ='10'
+let ba = 10
+console.log(typeof ab == typeof ba)
+
+//falsy
+//0
+//undefined
+//null
+
+//5-
+//1-true
+//2-true
+//3-false
+//4-false
+//5-true
+//6-true
+//7-false
+//8-false
+//9-false
+//10-true
+//11-false
+//12-
+let aab = 'python'
+let bba = 'jagron'
+ console.log(aab.length != bba.length)
+
+/*6-
+1-true
+2-false
+3-true
+4-true
+5-false
+6-true
+7-true
+8-false
+9-true
+10-true
+11-*/
+let aaaa = 'dragon'
+let bbbb = 'python'
+console.log(aaaa.includes('on') == bbbb.includes('on'))
+
+//7-
+//1-
+const now = new Date
+console.log(now.getFullYear())
+
+//2-
+const noww = new Date
+console.log(noww.getMonth())
+
+//3-
+const nowww = new Date
+console.log(nowww.getDate())
+
+//4-
+const noow = new Date
+console.log(noow.getDay())
+
+//5-
+const nnow = new Date
+console.log(nnow.getHours())
+
+//6-
+const nooow = new Date
+console.log(nooow.getMinutes())
+
+//7-
+const nnnow = new Date
+console.log(nnnow.getTime())
