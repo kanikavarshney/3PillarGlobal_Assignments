8056b05957e2ebf7ee65e00efa7af348de527763
kanikavarshney <kanika.varshney_cs19@gla.ac.in>
2022-06-17 01:50:25 +0530

    some styles

--- Level3/script.js
+++ Level3/script.js
@@ -14,3 +14,51 @@ function jsrun(){
         changeheight.className="javascript"
     }
     }
+    function htmlrun(){
+        var changeheight = document.getElementById("html")
+        if(changeheight.className == "html"){
+            changeheight.className = "htmll"
+        }else{
+            changeheight.className="html"
+        }
+        }
+    function reactrun(){
+        var changeheight = document.getElementById("react")
+        if(changeheight.className == "react"){
+            changeheight.className = "reactt"
+        }else{
+            changeheight.className="react"
+        }
+        }
+        function nativerun(){
+            var changeheight = document.getElementById("reactnative")
+            if(changeheight.className == "reactnative"){
+                changeheight.className = "reactnativee"
+            }else{
+                changeheight.className="reactnative"
+            }
+            }
+            function fullrun(){
+                var changeheight = document.getElementById("fullstack")
+                if(changeheight.className == "fullstack"){
+                    changeheight.className = "fullstackk"
+                }else{
+                    changeheight.className="fullstack"
+                }
+                }
+                function datarun(){
+                    var changeheight = document.getElementById("data")
+                    if(changeheight.className == "data"){
+                        changeheight.className = "dataa"
+                    }else{
+                        changeheight.className="data"
+                    }
+                    }
+                    function machinerun(){
+                        var changeheight = document.getElementById("machine")
+                        if(changeheight.className == "machine"){
+                            changeheight.className = "machinee"
+                        }else{
+                            changeheight.className="machine"
+                        }
+                        }
