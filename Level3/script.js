c225e7c15b3e6ea04a6fe18cfde77230e9234df2
kanikavarshney <kanika.varshney_cs19@gla.ac.in>
2022-06-16 22:20:13 +0530

    some files

---
+++ Level3/script.js
@@ -0,0 +1,16 @@
+function pyrun(){
+var changeheight = document.getElementById("python")
+if(changeheight.className == "python"){
+    changeheight.className = "pythonn"
+}else{
+    changeheight.className="python"
+}
+}
+function jsrun(){
+    var changeheight = document.getElementById("javascript")
+    if(changeheight.className == "javascript"){
+        changeheight.className = "javascriptt"
+    }else{
+        changeheight.className="javascript"
+    }
+    }
